aef4959ae35dff2dba6e7dae05b81b23dc004b36
Gamaliel Castro <Gamaliel Castro>
2017-04-08 10:38:21 -0500

    Calculadora

---
+++ Ejercicios/scripts.js
@@ -0,0 +1,14 @@
+			
+		function msg(){
+			var text = document.getElementById("textbox").value;
+			alert(text);
+		}
+
+		function suma(){
+			
+			var suma =  Number(document.getElementById("numero1").value) + Number(document.getElementById("numero2").value);
+
+			alert(suma);
+		}
+
+		

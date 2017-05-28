757162e843adcb9317b1a859e06ea52f31a105cb
Gamaliel Castro <gama_happy@hotmail.com>
2017-05-27 22:36:46 -0500

    terminado

--- resources/js/scripts.js
+++ resources/js/scripts.js
@@ -1,14 +1,18 @@
-			
-		function msg(){
-			var text = document.getElementById("textbox").value;
-			alert(text);
-		}
+			function msg() {
+			    var text = document.getElementById("textbox").value;
+			    alert(text);
+			}
 
-		function suma(){
-			
-			var suma =  Number(document.getElementById("numero1").value) + Number(document.getElementById("numero2").value);
-
-			alert(suma);
-		}
-
-		
+			function operacion() {
+			    var operacion;
+			    if (document.getElementById("seleccion").value == 'suma') {
+			        operacion = Number(document.getElementById("numero1").value) + Number(document.getElementById("numero2").value);
+			    } else if (document.getElementById("seleccion").value == 'resta') {
+			        operacion = Number(document.getElementById("numero1").value) - Number(document.getElementById("numero2").value);
+			    } else if (document.getElementById("seleccion").value == 'multiplicacion') {
+			        operacion = Number(document.getElementById("numero1").value) * Number(document.getElementById("numero2").value);
+			    } else {
+			        operacion = Number(document.getElementById("numero1").value) / Number(document.getElementById("numero2").value);
+			    }
+			    alert(operacion);
+			}
